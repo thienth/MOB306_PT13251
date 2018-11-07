f92d5fd67eba2e9f4b87813f5118ed826ab9fdba
thienth32 <thienth32@gmail.com>
2018-11-07 13:48:52 +0700

    update

--- screens/Home.js
+++ screens/Home.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { 
       StyleSheet, 
       Text, 
-      View
+      View,
+      TouchableOpacity
     } from 'react-native';
 
 export default class HomeScreen extends React.Component{
-
+  constructor(props) {
+    super(props);
+    this.goToCate = this.goToCate.bind(this);
+    
+  }
   static navigationOptions = {
       title: 'Trang chủ',
   };
+
+  goToCate(){
+    this.props.navigation.navigate('Category');
+  }
   
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <TouchableOpacity onPress={this.goToCate}>
+          <Text>Goto category screen</Text>
+        </TouchableOpacity>
       </View>
     );
   }
