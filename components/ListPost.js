c48e7e63f2effe9c2a429f344710271c2446f521
thienth32 <thienth32@gmail.com>
2018-11-21 13:23:33 +0700

    update

--- components/ListPost.js
+++ components/ListPost.js
@@ -3,21 +3,59 @@ import {
       StyleSheet, 
       Text, 
       View,
+      ScrollView,
+      Image,
       TouchableOpacity
     } from 'react-native';
-
+import firebaseConf from '../lib/firebaseConfig';
 export default class ListPost extends React.Component{
   constructor(props) {
     super(props);
-    
+    this.getPostList = this.getPostList.bind(this);
+
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    this.getPostList();
+  }
+
+  getPostList(){
+    var that = this;
+    firebaseConf.database().ref('posts/').on('value', function (snapshot) {
+        let posts = [];
+        snapshot.forEach((child) => {
+
+          let item = {
+            key: child.key,
+            data: child.val()
+          }
+          posts.push(item);
+        });
+
+        that.setState({posts});
+        console.log(that.state.posts);
+    });
   }
   
 
   render() {
+    console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
     return (
-      <View >
-        <Text>Danh sách bài viết</Text>
-      </View>
+      <ScrollView >
+        {this.state.posts.map(item => (
+          <View key={item.key} style={{marginTop: 5}}>
+            <Text style={{fontWeight: 'bold', fontSize: 25}}>
+              {item.data.title}
+            </Text>
+            <Image source={{uri: item.data.picture}} style={{width: '100%', height: 200}}/>
+            <Text>
+              {item.data.short_description}
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
     );
   }
 }
